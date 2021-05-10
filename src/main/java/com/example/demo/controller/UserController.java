c7c946a215774cf4604ee7b887c21922f73d9d6d
Huan <initxt@qq.com>
2021-05-10 14:42:27 +0800

    qwq

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -21,6 +21,7 @@ public class UserController {
 
     @GetMapping("/{id}")
     public String getUserById(@PathVariable Long id) {
+        System.out.println("hot-fix");
         System.out.println(123456);
         System.out.println(123);
         User byId = userService.getById(id);
